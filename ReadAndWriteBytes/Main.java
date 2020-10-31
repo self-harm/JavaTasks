f90e5bbb14e338f4e978f5028c5942ac34b5800f
self-harm <69676561+self-harm@users.noreply.github.com>
2020-10-31 04:49:17 +0300

    Add files via upload

---
+++ ReadAndWriteBytes/Main.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                String fileFirst = reader.readLine();
+                String fileSecond = reader.readLine();
+                String fileThird = reader.readLine();
+
+                try(FileInputStream inputStream = new FileInputStream(fileFirst);
+                    FileOutputStream outputStream1 = new FileOutputStream(fileSecond);
+                    FileOutputStream outputStream2 = new FileOutputStream(fileThird);)
+                {
+                    int halfOfTheFIle = (inputStream.available() + 1)/2;
+                    int count = 0;
+
+                    while (inputStream.available()>0){  //while fileFirst has unread bytes;
+                        if(inputStream.available()==0){ //close the streams if fileFirst doesn't have unread bytes;
+                            inputStream.close();
+                            outputStream1.close();
+                            outputStream2.close();
+                            break;
+                        }
+                        if(count < halfOfTheFIle) {   //reckon until count == halfOfTheFile;
+                            outputStream1.write(inputStream.read());
+                            count++;
+                        }
+                        else{  // (count >= halfOfTheFile);
+                            outputStream2.write(inputStream.read());
+                        }
+                    }
+                }
+    }
+}
+
