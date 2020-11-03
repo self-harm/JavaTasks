b1c5935f96f5689e756340693d87e524d825713a
self-harm <69676561+self-harm@users.noreply.github.com>
2020-11-03 19:11:40 +0300

    Add files via upload

---
+++ FileInputStream-FileOutputStream/FileReader-FileWriter/CountWords/Main.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.io.*;
+
+/*
+Считаем слово
+*/
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+
+        StringBuilder text = new StringBuilder();
+
+        try(FileReader fileReader = new FileReader(s)){
+            char currentChar;
+            while(fileReader.ready()) {
+                currentChar = (char) fileReader.read();
+                text.append(currentChar);
+            }
+        }
+
+        int count=0;
+
+        String replacedString = text.toString().replaceAll("\\p{P}", " ").replaceAll("\\s", " ");
+
+        for(String a: replacedString.split(" ")){
+            if(a.matches("world")){
+                count++;
+            }
+        }
+
+        reader.close();
+        System.out.println(count);
+
+    }
+}
