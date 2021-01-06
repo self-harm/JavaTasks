0ed573187df03b922ff121a8727683ed947cf9e8
self-harm <lightgraywrite@gmail.com>
2021-01-06 22:18:52 +0300

    Create Main.java

---
+++ creatingTree/Main.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new CustomTree();
+
+        for (int i = 1; i < 16; i++) {
+            list.add(String.valueOf(i));
+        }
+
+        System.out.println("The list size is " + list.size());
+        System.out.println("The expected parent is 3. The actual parent is " + ((CustomTree) list).getParent("8"));
+        System.out.println("The expected parent is null. The actual parent is " + ((CustomTree) list).getParent("20"));
+    }
+}
