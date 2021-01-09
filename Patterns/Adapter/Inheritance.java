8165080286824a6adf87c3fec8a7576bb3f7f602
self-harm <lightgraywrite@gmail.com>
2021-01-09 23:11:17 +0300

    Create Inheritance.java

---
+++ Patterns/Adapter/Inheritance.java
@@ -0,0 +1,54 @@
+// Target
+public interface Chief
+{
+  public Object makeBreakfast();
+  public Object makeLunch();
+  public Object makeDinner();
+}
+
+// Adaptee
+public class Plumber
+{
+  public Object getScrewNut()
+  { ... }
+  public Object getPipe()
+  { ... }
+  public Object getGasket()
+  { ... }
+}
+
+// Adapter
+public class ChiefAdapter extends Plumber implements Chief
+{
+  public Object makeBreakfast()
+  {
+    return getGasket();
+  }
+  public Object makeLunch()
+  {
+    return getPipe();
+  }
+  public Object makeDinner()
+  {
+    return getScrewNut();
+  }
+}
+
+// Client
+public class Client
+{
+  public static void eat(Object dish)
+  { ... }
+
+  public static void main(String[] args)
+  {
+    Chief ch = new ChiefAdapter();
+    Object dish = ch.makeBreakfast();
+    eat(dish);
+    dish = ch.makeLunch();
+    eat(dish);
+    dish = ch.makeDinner();
+    eat(dish);
+    callAmbulance();
+  }
+}
