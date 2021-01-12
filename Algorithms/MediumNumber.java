fe7cfb62b73eb1b62b29711ad3cb5b35e8bf56de
self-harm <lightgraywrite@gmail.com>
2021-01-12 21:02:26 +0300

    Update MediumNumber.java

--- Algorithms/MediumNumber.java
+++ Algorithms/MediumNumber.java
@@ -13,6 +13,7 @@ import java.util.Arrays;
 
 /* 
 Почитать в инете про медиану выборки
+первое решение
 */
 
 public class MediumNumber {
@@ -47,5 +48,51 @@ public class MediumNumber {
                 }
             }
         return array;
+    }
+}
+
+
+/* 
+второе решение
+*/
+import java.util.Arrays;
+import java.util.Comparator;
+
+/* 
+Почитать в инете про медиану выборки
+*/
+
+public class MainSecond {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static Integer[] sort(Integer[] array) {
+        final double mediana = getMediana(array);
+
+        Arrays.sort(array, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                double v1 = o1.intValue() - mediana;
+                double v2 = o2.intValue() - mediana;
+                return (int) ((v1 * v1 - v2 * v2) * 100);
+            }
+        });
+
+        return array;
+    }
+
+    private static double getMediana(Integer[] array) {
+        Arrays.sort(array);
+        double res;
+        int length = array.length;
+
+        if (length % 2 == 1) {
+            res = array[length / 2];
+        } else {
+            res = (array[length / 2 - 1] + array[length / 2]) / 2.;
+        }
+        return res;
     }
 }
