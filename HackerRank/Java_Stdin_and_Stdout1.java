f18a00e2757e68fe009f33fdeb43b193ecd08df1
self-harm <lightgraywrite@gmail.com>
2021-03-30 23:01:24 +0300

    Update Java_Stdin_and_Stdout1.java

--- HackerRank/Java_Stdin_and_Stdout1.java
+++ HackerRank/Java_Stdin_and_Stdout1.java
@@ -1,11 +1,36 @@
-public class Solution {
+package io.harmed;
+
+import java.util.*;
+
+public class Main {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
         
-      while (scan.hasNext()){
-            int a = scan.nextInt();
-            System.out.println(a);
+        /** 1s task*/
+        
+//        Scanner scan = new Scanner(System.in);
+//
+//        while (scan.hasNext()){
+//            int a = scan.nextInt();
+//            System.out.println(a);
+
+
+        /** 2d task*/
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        Double d = scan.nextDouble();
+        /*: If you use the nextLine() method immediately following the nextInt() method,
+         recall that nextInt() reads integer tokens; because of this, the last newline
+         character for that line of integer input is still queued in the input buffer
+         and the next nextLine() will be reading the remainder of the integer line (which is empty).
+
+         we need to clear the buffer by using scan.nextLine();*/
+        scan.nextLine();
+        String s = scan.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+
         }
     }
-}
